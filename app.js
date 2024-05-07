0007c15a3e6e246b414ca8515b32d315193fde5f
Byte More <charles.aggasid01@gmail.com>
2024-05-06 21:16:59 -0500

    adding comments to app.js

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
-const express = require('express');
-const e = require("express");
-const path = require("path");
+// Importing necessary modules
+const express = require('express'); // Importing Express framework
+const path = require("path"); // Importing path module to handle file paths
 
+// Creating an Express application instance
+const app = express(); // Creating an Express application
 
-
-
-
-
-
-
-
-
-const app = express();
-
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+// Setting view engine to EJS and specifying views directory
+app.set('view engine', 'ejs'); // Setting the view engine to EJS
+app.set('views', path.join(__dirname, 'views')); // Specifying the directory for views
 
 //------------------------SAMPLE PAGE-----------------------
+// Handling GET request to the root URL
 app.get('/', (req, res) => {
-    // res.send('HELLO FROM YELPCAMP')
+    // Rendering the "home" view
     res.render("home");
-})
-
-
-
+});
 
 //------------------------listen to port-----------------------
+// Listening on port 3000 for incoming requests
 app.listen(3000, () => {
-    console.log('PORT 3000')
-})
+    console.log('Server is running on PORT 3000'); // Logging that the server is running
+});
