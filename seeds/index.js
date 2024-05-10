652f1135f2e8add064de6ed471b524c0e6ba84f6
Byte More <charles.aggasid01@gmail.com>
2024-05-10 08:41:29 -0500

    43
    client side validation skip

--- seeds/index.js
+++ seeds/index.js
@@ -3,15 +3,10 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    // useNewUrlParser: true,
-    // useCreateIndex: true,
-    // useUnifiedTopology: true
-});
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
 const db = mongoose.connection;
-
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
